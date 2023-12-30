b9059a994f51548efb60dbfd47af49b75674af60
gabrielmj23 <gabrielmj2312@gmail.com>
2023-12-30 19:32:48 -0400

    Coerce para la recomendacion

--- src/schemas/recomendacion.ts
+++ src/schemas/recomendacion.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const recomendacionSchema = z.object({
-  idRespuesta: z.number().int().positive(),
+  idRespuesta: z.coerce.number().int().positive(),
   recomendacion: z.string().min(1).max(512, {
     message: "La recomendación debe tener menos de 512 caracteres",
   }),
